95ed6551f3b778552d775a567a732f146a0f3329
Manuel Roca <manuelrvalla@gmail.com>
2025-05-23 21:20:38 -0300

    Cambio fondo inicio juego

--- Dependencias/hud.h
+++ Dependencias/hud.h
@@ -527,8 +527,6 @@ GLuint HUD::cargarTexturaCarta(const char* archivo) {
     glBindTexture(GL_TEXTURE_2D, textura);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_NEAREST);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
     gluBuild2DMipmaps(GL_TEXTURE_2D, GL_RGBA, this->carta_w, this->carta_h, GL_BGRA, GL_UNSIGNED_BYTE, datos);
     glTexEnvf(GL_TEXTURE_ENV, GL_TEXTURE_ENV_MODE, GL_MODULATE);
 
